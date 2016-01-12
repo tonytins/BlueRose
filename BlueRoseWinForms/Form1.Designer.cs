5f8be2561f00bd16dec95938f6cea3fd62788615
Zack Casey <aidancheddar@gmail.com>
2016-01-11 22:35:03 -0500

    Initial update functionality
    Not ready for production yet

--- BlueRoseWinForms/Form1.Designer.cs
+++ BlueRoseWinForms/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.freeSONewsBtn = new System.Windows.Forms.Button();
             this.brNewsBtn = new System.Windows.Forms.Button();
             this.fsoWeb = new System.Windows.Forms.WebBrowser();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dwnPrgBar = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // playBtn
@@ -84,20 +86,46 @@
             this.fsoWeb.Size = new System.Drawing.Size(760, 508);
             this.fsoWeb.TabIndex = 4;
             // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(174, 526);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // dwnPrgBar
+            // 
+            this.dwnPrgBar.Location = new System.Drawing.Point(255, 526);
+            this.dwnPrgBar.Name = "dwnPrgBar";
+            this.dwnPrgBar.Size = new System.Drawing.Size(302, 23);
+            this.dwnPrgBar.TabIndex = 6;
+            this.dwnPrgBar.Click += new System.EventHandler(this.dwnPrgBar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(99)))), ((int)(((byte)(168)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dwnPrgBar);
+            this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.fsoWeb);
             this.Controls.Add(this.brNewsBtn);
             this.Controls.Add(this.freeSONewsBtn);
             this.Controls.Add(this.devBtn);
             this.Controls.Add(this.playBtn);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Blue Rose";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -109,6 +137,8 @@
         private System.Windows.Forms.Button freeSONewsBtn;
         private System.Windows.Forms.Button brNewsBtn;
         private System.Windows.Forms.WebBrowser fsoWeb;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ProgressBar dwnPrgBar;
     }
 }
 
--- BlueRoseWinForms/Form1.cs
+++ BlueRoseWinForms/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows.Forms;
 using BlueRoseShared;
 
@@ -48,11 +43,61 @@ namespace BlueRoseWinForms
             {
                 fsoWeb.Navigate(new Uri(url, UriKind.RelativeOrAbsolute));
             }
-            catch
+            catch (Exception ex)
             {
-                MessageBox.Show("Page Not Found.");
+                MessageBox.Show(ex.Message);
                 fsoWeb.Navigate(new Uri("http://forum.freeso.org/", UriKind.RelativeOrAbsolute));
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+
+            try
+            {
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(clDownProgChanged);
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(clDownFileCompleted);
+
+                client.DownloadFileAsync(new Uri("http://servo.freeso.org/repository/download/ProjectDollhouse_TsoClient/262:id/dist-196.zip"), Environment.CurrentDirectory);
+
+                btnUpdate.Text = "Downloading";
+                btnUpdate.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                btnUpdate.Text = "ERROR";
+                btnUpdate.Enabled = false;
+            }
+
+            
+        }
+
+        void clDownProgChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+
+            dwnPrgBar.Value = int.Parse(Math.Truncate(percentage).ToString());
+        }
+
+        void clDownFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            btnUpdate.Text = "Update";
+            btnUpdate.Enabled = true;
+        }
+
+        private void dwnPrgBar_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }

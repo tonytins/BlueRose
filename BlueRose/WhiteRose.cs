4e9c906de60c15cc7517f9b4859284b66cbba8dd
Tony Bark <35226681+tonytins@users.noreply.github.com>
2022-07-19 04:43:18 -0400

    Public Domain With the exception of Wildcard.cs, I hereby waive all code related to Blue Rose in the public domain.

--- BlueRose/WhiteRose.cs
+++ BlueRose/WhiteRose.cs
@@ -1,20 +1,4 @@
-﻿// Copyright(C) 2016  Blue Rose Project
-
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
@@ -212,7 +196,7 @@ namespace BlueRose
         /// </summary>
         public static void ZipGcCompat()
         {
-            
+
             Wildcard wildZip = new Wildcard("*.zip", RegexOptions.IgnoreCase);
             string[] files = Directory.GetFiles(Environment.CurrentDirectory);
 
@@ -245,7 +229,7 @@ namespace BlueRose
                 }
             }
         }
-        
+
         /// <summary>
         /// Detects for any present zips and unpacks them.
         /// Uses Code Project's Wildcard class.
